26c5954229a155700946e581fb287762a24dbe59
NPH82 <nich1982@gmail.com>
2018-02-04 15:03:36 -0500

    First commit

---
+++ src/layouts/index.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+import Media from 'react-media'
+
+import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
+import './index.css'
+import '../styles/layout-overide.css';
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="Nic's Blog"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Header />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 980,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        height: '100%'
+      }}
+    >
+      <Media query={{ maxWidth: 848 }}>
+        {matches =>
+          matches ? (
+            <div
+              style={{
+                margin: '0 auto',
+                maxWidth: 980,
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                height: '100%',
+                padding: '25px'
+              }}
+            >
+              <div style={{ flex: 1 }}>{children()}</div>
+            </div>
+          ) : (
+            <div
+              style={{
+                margin: '0 auto',
+                maxWidth: 980,
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                height: '100%',
+                padding: '25px'
+              }}
+            >
+              <div style={{ flex: 2.5, paddingRight: '30px' }}>
+                {children()}
+              </div>
+              
+              <div style={{ flex: 1 }}>
+                <Sidebar
+                  title='Blog'
+                  description='Notes and information I have gained through my investigation of web development.'
+                /> 
+                <Sidebar
+                  title='About Author'
+                  description='I am a full-stack web developer always exploring new avenues to create and share my knowledge of web development technologies and practices.'
+                />
+              </div>
+            </div>
+          )
+        }
+      </Media>
+    </div>
+  </div>
+);
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
---
+++ src/pages/02-04-2018-first-post/index.md
@@ -0,0 +1,11 @@
+---
+path: "/hello-world"
+date: "2018-02-04"
+title: "My First Post"
+---
+
+This is my first post to experiment with Gatsby and React.  I'm excited to share information that I will be gathering as I search for employment.
+
+I am planning on deploying to Netlify and updating my posts at least once a week if not more.
+
+I am looking forward to the direction this blog takes me.
